264e7207c530906e46605bd252cca61fc36d588b
Alex Baucom <alexbaucom17@gmail.com>
2017-01-09 17:04:19 -0500

    super simple behavior code working in simulation

--- catkin_ws/src/husky/src/simpleBehavior.py
+++ catkin_ws/src/husky/src/simpleBehavior.py
@@ -4,35 +4,41 @@ import tf
 import actionlib
 from nav_msgs.msg import OccupancyGrid
 from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction, MoveBaseResult
+from geometry_msgs.msg import PoseWithCovarianceStamped
 
 def map_callback(data):
 	#do something here
-	pass
+	rospy.loginfo("I got a map")
+
+def pose_callback(data):
+	#do something
+	rospy.loginfo("I got a pose")
 
 
 #run main behavior code
 def run_node():
 	rospy.init_node('huskyBehvaior')
-	mapSub = rospy.Subscirber("map",OccupancyGrid,map_callback)
-	tfListener = tf.TransformListener()
+	mapSub = rospy.Subscriber("map",OccupancyGrid,map_callback)
+	poseSub = rospy.Subscriber("amcl_pose",PoseWithCovarianceStamped,pose_callback)
 	mb_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 	mb_client.wait_for_server() 
 
 	while not rospy.is_shutdown():
 
-		#get current location in map coordinates
-		(trans,rot) = tfListener.lookupTransform('/map', '/odom', rospy.Time(0))
 		#Create goal
 		actionGoal = MoveBaseGoal()
-		actionGoal.target_pose.header.frame_id = "map"
+		actionGoal.target_pose.header.frame_id = "base_link"
 		actionGoal.target_pose.header.stamp = rospy.get_rostime()
 		
 		#Set actual goal cordinates here based on map, location, camera, etc. Might consider subscribing to costmaps as well
 
+		actionGoal.target_pose.pose.position.x = 4.0;
+		actionGoal.target_pose.pose.orientation.w = 1.0;
+
 		mb_client.send_goal(actionGoal)
 		mb_client.wait_for_result()
 
-		rospy.spin()
+		rospy.loginfo("Behavior is alive")
 
 
 
